f63ae3b6ce93bdd7af2c1878600d8a4ccf6d4318
EricChukoltyansky <kazohelmet@gmail.com>
2021-12-19 13:23:20 +0200

    Jokes button

--- src/components/12.1/Joke.jsx
+++ src/components/12.1/Joke.jsx
@@ -4,23 +4,79 @@ import Button from "./Button";
 import Search from "./Search";
 
 export default class Joke extends Component {
-  state = { jokes: "" };
+  state = { jokes: "", categories: [], searchCate: "" };
 
   onButtonSubmit = async () => {
     const response = await axios.get("https://api.chucknorris.io/jokes/random");
-    console.log(response.data.value);
+    console.log(response.data);
     this.setState({
       jokes: response.data.value,
     });
-    console.log(this.state.jokes);
   };
+
+  onButtonCategoriesSubmit = async () => {
+    const response = await axios.get(
+      "https://api.chucknorris.io/jokes/categories"
+    );
+
+    this.setState({
+      categories: response.data,
+    });
+  };
+
+  getJokeFromCategories = async (value) => {
+    const response = await axios.get(
+      `https://api.chucknorris.io/jokes/random?category=${value}`
+    );
+
+    this.setState({
+      jokes: response.data.value,
+    });
+  };
+
+  //   onSearchInput = async (term) => {
+  //     const response = await axios.get(
+  //       "https://api.chucknorris.io/jokes/categories",
+  //       {
+  //         params: { query: term },
+  //       }
+  //     );
+
+  //     this.setState({
+  //       searchCate: response.data,
+  //     });
+  //   };
 
   render() {
     return (
       <>
-        <Button onClickFunction={this.onButtonSubmit} text="Click me" />
-        <div>{this.state.jokes}</div>
-        <Search />
+        <Button onClickFunction={this.onButtonSubmit} text="Random" />
+        <Button
+          onClickFunction={this.onButtonCategoriesSubmit}
+          text=" Send Categories To State"
+        />
+        <Button text="Present Categoires" />
+        <div>Random Joke:{this.state.jokes}</div>
+
+        <div>
+          {this.state.categories.map((cat) => {
+            return (
+              <button
+                key={cat}
+                onClick={(e) =>
+                  this.getJokeFromCategories(e.target.textContent)
+                }
+              >
+                {cat}
+              </button>
+            );
+          })}
+        </div>
+        <Button onClickFunction={this.onButtonSubmit} />
+        <Search
+          onClickFunction={this.onButtonCategoriesSubmit}
+          value={this.onButtonCategoriesSubmit}
+        />
       </>
     );
   }
--- src/components/12.1/Search.jsx
+++ src/components/12.1/Search.jsx
@@ -4,7 +4,13 @@ export default class Search extends Component {
   render() {
     return (
       <div>
-        <input type="search" name="search" id="search" />
+        <input
+          type="search"
+          name="search"
+          id="search"
+          onSubmit={this.props.onClickFunction}
+          value={this.props.searchValue}
+        />
       </div>
     );
   }
